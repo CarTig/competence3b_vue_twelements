5b88eae9f6e68879f63c85b2c2826bb816a7d2aa
CarTig <carolle.t.n@gmail.com>
2022-11-28 14:33:32 +0100

    refresh dans configjs font tomarik line

--- herborist/src/components/Decouverte.vue
+++ herborist/src/components/Decouverte.vue
@@ -2,7 +2,7 @@
     <section class="font-gustavo mt-4 md:mt-8">
         <div class=" flex flex-col items-center  md:flex-row md:justify-evenly lg:space-x-1">
             <div class="mb-4 border-solid border-2 shadow-lg rounded-lg h-20 w-60 py- text-center bg-amber-200 lg:h-80 lg:w-80 lg:rounded-full lg:py-28"> 
-                <p class="font-bold text-white text-2xl lg:text-5xl">Découverte des plantes</p>
+                <p class="font-bold text-white text-2xl lg:text-5xl">Découverte <br> des plantes</p>
             </div>
             <div class="mb-4 border-solid border-2 shadow-lg rounded-lg h-20 w-60 py-6 text-center bg-amber-200 lg:h-80 lg:w-80 lg:rounded-full lg:py-28"> 
                 <p class="font-bold text-white text-2xl lg:text-5xl">Plantes sur mesures</p>
